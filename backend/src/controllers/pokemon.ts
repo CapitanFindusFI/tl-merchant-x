58cd4202106cee725e8f48cac08254a3ed382163
CapitanFindusFI <am95.dev@gmail.com>
2022-02-18 16:06:47 +0100

    updated pokemon controller with express-validation for empty string

--- backend/src/controllers/pokemon.ts
+++ backend/src/controllers/pokemon.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { param, validationResult } from "express-validator";
 import Logger from '../lib/logger';
 import pokedex from '../services/pokedex';
 
@@ -8,10 +9,17 @@ const logger = Logger.getLogger().child({
 });
 
 const router = express.Router();
+const validation = param("name").isString().trim().toLowerCase().notEmpty();
 
 router.get(
     "/:name",
+    validation,
     async (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
+
         const { name } = req.params;
         logger.debug(`Obtaining informations for pokemon: ${name}`);
         try {
