c0087bd68d813762f2d67cd4850b8714e9fc6f82
CapitanFindusFI <am95.dev@gmail.com>
2022-02-21 15:57:35 +0100

    installed jest, ts-jest and created configuration file

---
+++ backend/jest.config.js
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleFileExtensions: ["js", "ts", "json", "node"],
+  rootDir: "./src",
+  roots: ["./src"],
+};
