d2766227548424c327de27e3ffe01cb5482fc50c
CapitanFindusFI <am95.dev@gmail.com>
2022-02-22 19:10:15 +0100

    integrated axios and created a few configuration constants

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,17 +4,41 @@ import PageTitle from "./components/page-title";
 import SearchForm from "./components/search-form";
 import BoxedContainer from "./layout/boxed-container";
 import FullwidthContainer from "./layout/fullwidth-container";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import httpClient from "./lib/api";
 
 const App = () => {
-  const onFormSubmit = (value: string) => {
-    console.log(`Value set: ${value}`);
+  const dispatch = useAppDispatch();
+  const { error, result, query, isLoading } = useAppSelector(
+    (state) => state.search
+  );
+
+  const setLoading = (loading: boolean) =>
+    dispatch({
+      type: "@search/setLoading",
+      payload: loading,
+    });
+
+  const onFormSubmit = async () => {
+    console.log(`Value set: ${query}`);
+    try {
+      setLoading(true);
+
+      const { data } = await httpClient.get<any>(`/${query}`);
+      console.log(data);
+    } catch (e) {
+      dispatch({
+        type: "@search/setError",
+        payload: e,
+      });
+    }
   };
 
   return (
     <FullwidthContainer>
       <BoxedContainer>
         <PageTitle>Pokedex</PageTitle>
-        <SearchForm onSubmit={onFormSubmit} />
+        <SearchForm onFormSubmit={onFormSubmit} />
       </BoxedContainer>
     </FullwidthContainer>
   );
--- frontend/src/components/search-form/index.tsx
+++ frontend/src/components/search-form/index.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
-import useDebounce from "../../hooks/use-debounce";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import * as S from "./styles";
 
@@ -14,23 +13,15 @@ const SearchForm: React.FC<PropsType> = (props: PropsType) => {
   const { onFormSubmit } = props;
 
   const [t] = useTranslation();
+
   const dispatch = useAppDispatch();
   const { query, isLoading } = useAppSelector((state) => state.search);
-
-  const debouncedTerm = useDebounce(query, 300);
-
-  useEffect(() => {
-    if (debouncedTerm) {
-      console.log(debouncedTerm);
-    }
-  }, [debouncedTerm]);
 
   const isSubmitDisabled = useMemo(() => {
     return query.length <= 2 || isLoading;
   }, [query, isLoading]);
 
   const onInputChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    e.preventDefault();
     dispatch({
       type: "@search/setQuery",
       payload: e.currentTarget.value,
